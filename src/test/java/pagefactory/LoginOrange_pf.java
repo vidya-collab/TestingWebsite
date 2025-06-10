35b51fe5648f938975588169994218835a76a031
vidya-collab <vidya.kushte@gmail.com>
2025-06-10 14:16:14 -0500

    login page code

---
+++ src/test/java/pagefactory/LoginOrange_pf.java
@@ -0,0 +1,79 @@
+package pagefactory;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import utils.ExcelReader;
+import utils.ConfigReader;
+import utils.DriverManager;
+
+public class LoginOrange_pf {
+	
+	
+	WebDriver driver= DriverManager.getdriver();
+	ConfigReader configFileReader=DriverManager.configReader();
+
+	
+	
+	//input[@name='username']
+	//input[@name='password']
+	
+	
+	@FindBy(xpath = "//input[@name='username']") WebElement username;
+	@FindBy(xpath = "//input[@name='password']") WebElement password;
+	@FindBy(xpath = "//button[contains(@class,'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button')]") WebElement login;
+	
+	ExcelReader reader = new ExcelReader();      // add for data driven 
+	String Excelpath = ConfigReader.excelpath(); // add for data driven 
+	
+	public LoginOrange_pf() {
+		
+		PageFactory.initElements(driver , this);
+
+	}
+	
+	public String getusernamefromExcel(String Sheetname, int Rownumber)  throws InvalidFormatException, IOException, org.apache.poi.openxml4j.exceptions.InvalidFormatException  {	
+
+		List<Map<String,String>> testData = 
+				reader.getData(Excelpath, Sheetname);
+
+		String Uname  = testData.get(Rownumber).get("Username");
+		return Uname;
+	}
+
+	public String getpasswordfromExcel(String Sheetname, int Rownumber)  throws InvalidFormatException, IOException, org.apache.poi.openxml4j.exceptions.InvalidFormatException  {	
+
+		List<Map<String,String>> testData = 
+				reader.getData(Excelpath, Sheetname);
+
+		String Pd  = testData.get(Rownumber).get("Password");
+		return Pd;
+	}
+	
+	
+	public void  login2(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+		List<Map<String,String>> testData = 
+				reader.getData(Excelpath, "Loginsheet");
+		Uname = testData.get(0).get("Username");
+		Pd = testData.get(0).get("Password");
+		//username.clear();
+		username.sendKeys(Uname);
+		//password.clear();
+		password.sendKeys(Pd);
+		login.click();	
+		}
+	
+	
+	
+
+}
--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,5 +1,52 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+	private static Properties properties;
+	private final static String propertyFilePath = "src/test/resources/config/Config.properties";
+
+	
+	public ConfigReader() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(propertyFilePath));
+			properties = new Properties();
+			try {
+				properties.load(reader);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+		}
+
+	}
+
+	public static String getApplicationUrl() {
+		String url = properties.getProperty("url");
+		System.out.println(url);
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("url not specified in the Configuration.properties file.");
+	}
+	public static String excelpath() {
+		String path = properties.getProperty("path");
+		System.out.println(path);
+		if (path != null)
+			return path;
+		else
+			throw new RuntimeException("url not specified in the Configuration.properties file.");
+	
+	
+	}
+
 
 }
