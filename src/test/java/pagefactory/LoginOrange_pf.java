7d86cbc59a133ee5a732edfb48dbb028b46d000c
vidya-collab <vidya.kushte@gmail.com>
2025-07-10 15:01:31 -0500

    Orange hrm login page tseting code with data driven

--- src/test/java/pagefactory/LoginOrange_pf.java
+++ src/test/java/pagefactory/LoginOrange_pf.java
@@ -3,45 +3,52 @@ package pagefactory;
 
 import java.io.IOException;
 
+
 import java.util.List;
 import java.util.Map;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-
+import io.netty.handler.timeout.TimeoutException;
 import utils.ExcelReader;
 import utils.ConfigReader;
 import utils.DriverManager;
+import java.time.Duration;
+
 
 public class LoginOrange_pf {
 	
 	
 	WebDriver driver= DriverManager.getdriver();
 	ConfigReader configFileReader=DriverManager.configReader();
+	ExcelReader reader = new ExcelReader();      // add for data driven 
+	String Excelpath = ConfigReader.excelpath(); // add for data driven 
 
-
-	//input[@placeholder='Username']
-	//input[@placeholder='Password']
-	//button[@type='submit']
-
+	//-----------------@FIndBy method for locartors(xpath)-------------------------
 	
 	@FindBy(xpath = "//input[@name='username']") WebElement username;
 	@FindBy(xpath = "//input[@name='password']") WebElement password;
-	@FindBy(xpath = "//button[contains(@class,'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button')]") WebElement login;
+	@FindBy(xpath = "//span[text()='Dashboard']")WebElement DashBoard;
+	@FindBy(xpath = "//p[contains(@class,'oxd-alert-content-text') and text()='Invalid credentials']")WebElement Errormsg;
+	@FindBy(xpath = "//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")WebElement LoginButton;
+
+	//-------------------------------------constructor for initializing driver---------
 	
-	ExcelReader reader = new ExcelReader();      // add for data driven 
-	String Excelpath = ConfigReader.excelpath(); // add for data driven 
-	
-	public LoginOrange_pf() {
+	public LoginOrange_pf() {    
 		
 		PageFactory.initElements(driver , this);
 
 	}
+	
+	//-------------- code for getting value of username and password from excel sheet------
 	
 	public String getusernamefromExcel(String Sheetname, int Rownumber)  throws InvalidFormatException, IOException, org.apache.poi.openxml4j.exceptions.InvalidFormatException  {	
 
@@ -61,8 +68,9 @@ public class LoginOrange_pf {
 		return Pd;
 	}
 	
+	//--------------- sending value to text box of user name and password and click login button-----------
 	
-	public void  login2(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+	public void  loginOrange(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
 		List<Map<String,String>> testData = 
 				reader.getData(Excelpath, "Loginsheet");
 		Uname = testData.get(0).get("Username");
@@ -71,10 +79,121 @@ public class LoginOrange_pf {
 		username.sendKeys(Uname);
 		//password.clear();
 		password.sendKeys(Pd);
-		login.click();	
+		LoginButton.click();	
 		}
 	
+	//----------------Validate Dashborad page is comimg-------------------------------------------
 	
+	public String validateDashBoardpage() {
+		String dashBoard=DashBoard.getText();
+		return dashBoard;
+
+	}
+	
+	//-----------------for sending invalid credentials invalid username --------------
+	
+	public void invalidLoginOrange1(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+		List<Map<String,String>> testData = 
+				reader.getData(Excelpath, "Loginsheet");
+		Uname = testData.get(1).get("Username");
+		Pd = testData.get(1).get("Password");
+		//username.clear();
+		username.sendKeys(Uname);
+		//password.clear();
+		password.sendKeys(Pd);
+		LoginButton.click();
+			
+		}
+	
+	//-----------------for sending invalid credentials invalid password --------------
+	
+		public void invalidLoginOrange2(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+			List<Map<String,String>> testData = 
+					reader.getData(Excelpath, "Loginsheet");
+			Uname = testData.get(2).get("Username");
+			Pd = testData.get(2).get("Password");
+			//username.clear();
+			username.sendKeys(Uname);
+			//password.clear();
+			password.sendKeys(Pd);
+			LoginButton.click();
+				
+			}
+	
+	
+		//-----------------for sending invalid credentials empty username --------------
+		
+		public void invalidLoginOrange3(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+			List<Map<String,String>> testData = 
+					reader.getData(Excelpath, "Loginsheet");
+			Uname = testData.get(3).get("Username");
+			Pd = testData.get(3).get("Password");
+			//username.clear();
+			username.sendKeys(Uname);
+			//password.clear();
+			password.sendKeys(Pd);
+			LoginButton.click();
+				
+			}
+	
+		//-----------------for sending invalid credentials empty password --------------
+		
+		public void invalidLoginOrange4(String Uname, String Pd ) throws org.apache.poi.openxml4j.exceptions.InvalidFormatException, IOException{
+			List<Map<String,String>> testData = 
+					reader.getData(Excelpath, "Loginsheet");
+			Uname = testData.get(4).get("Username");
+			Pd = testData.get(4).get("Password");
+			//username.clear();
+			username.sendKeys(Uname);
+			//password.clear();
+			password.sendKeys(Pd);
+			LoginButton.click();
+				
+			}
+	//-------------for catching error message invalid credentials ---------//
+	
+	
+	public boolean isInvalidCredentialDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    try {
+	        WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+	            By.xpath("//p[contains(@class,'oxd-alert-content-text') and text()='Invalid credentials']")
+	        ));
+	        return errorElement.isDisplayed();
+	    } catch (TimeoutException e) {
+	        return false; // Element not found within timeout
+	    }
+	}
 	
 
+	//---------- for catching reqiured msg for username--------------------------------------------------------------------
+
+
+
+	public boolean isRequiredforusernameDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    try {
+	        WebElement req1Element = wait.until(ExpectedConditions.visibilityOfElementLocated(
+	            By.xpath("//span[text()='Required']")
+	        ));
+	        return req1Element.isDisplayed();
+	    } catch (TimeoutException e) {
+	        return false; // Element not found within timeout
+	    }
+	}
+	//---------- for catching reqiured msg for password--------------------------------------------------------------------
+
+
+
+		public boolean isRequiredforpasswordDisplayed() {
+		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		    try {
+		        WebElement req1Element = wait.until(ExpectedConditions.visibilityOfElementLocated(
+		            By.xpath("//span[text()='Required']")
+		        ));
+		        return req1Element.isDisplayed();
+		    } catch (TimeoutException e) {
+		        return false; // Element not found within timeout
+		    }
+		}
 }
--- src/test/resources/features/Loginorange.feature
+++ src/test/resources/features/Loginorange.feature
@@ -1,19 +1,54 @@
 @TestingWebsite
-Feature: Testing Login
-  I want to Enter details and Login
+Feature: Testing Login ,I want to Enter details and Login
 
- # @Login1
-  #Scenario Outline: Verify that user is able to land on Login Page
-   # Given The user is on the OrangeHRMLogin Page
-    #When The user enters username and password and click the login button
-    #Then The user should be redirected to dashboard page
-    
-  @Login2
+  @Login1
   Scenario Outline: user able to land on Dashboard page after entering valid Username and Password fields from excel sheet
-   Given The users is on the OrangeHRMLogin in Page
-   When The users enters login button after entering valid "<Sheetname>" and <Rownumber>
-   Then The user should be redirected to dashboard page of orange HRM
+    Given The users is on the OrangeHRMLogin in Page
+    When The users enters login button after entering valid "<Sheetname>" and <Rownumber>
+    Then The user should be redirected to dashboard page of orange HRM
 
     Examples: 
-      | Sheetname | Rownumber |
-      | Loginsheet     |         0 |
+      | Sheetname  | Rownumber |
+      | Loginsheet |         0 |
+
+  @Login2
+  Scenario Outline: user able to land on Dashboard page after entering invalid Username and Password fields from excel sheet
+    Given The users is on the OrangeHRMLogin in Page
+    When The users clicks login button after entering invalid username "<Sheetname>" and <Rownumber>
+    Then The user should get meesage invalid "credentials"
+
+    Examples: 
+      | Sheetname  | Rownumber |
+      | Loginsheet |         1 |
+
+  @Login3
+  Scenario Outline: user able to land on Dashboard page after entering invalid Username and Password fields from excel sheet
+    Given The users is on the OrangeHRMLogin in Page
+    When The users clicks login button after entering invalid password "<Sheetname>" and <Rownumber>
+    Then The user should get meesage invalid "credentials"
+
+    Examples: 
+      | Sheetname  | Rownumber |
+      | Loginsheet |         2 |
+
+ @Login4
+  Scenario Outline: user able to land on Dashboard page after entering invalid Username and Password fields from excel sheet
+    Given The users is on the OrangeHRMLogin in Page
+    When The users clicks login button after entering empty username "<Sheetname>" and <Rownumber>
+    Then The user should get meesage Required for username
+
+    Examples: 
+      | Sheetname  | Rownumber |
+      | Loginsheet |         3 |
+      
+  @Login5
+  Scenario Outline: user able to land on Dashboard page after entering invalid Username and Password fields from excel sheet
+    Given The users is on the OrangeHRMLogin in Page
+    When The users clicks login button after entering empty passowrd "<Sheetname>" and <Rownumber>
+    Then The user should get meesage Required for password
+
+    Examples: 
+      | Sheetname  | Rownumber |
+      | Loginsheet |         4 |
+      
+      
